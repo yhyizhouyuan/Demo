b84561982447fa93faa4c7652608394777791c87
yhyizhouyuan <2017767351@qq.com>
2021-10-01 09:32:34 +0800

    first commit

---
+++ spring-annotation/src/main/java/com/galaxy/bean/Test.java
@@ -0,0 +1,20 @@
+package com.galaxy.bean;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+/**
+ * <p>TODO</p>
+ *
+ * @author Hance
+ * @version V1.0.0
+ * @date 2021/10/1 9:30
+ */
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Test {
+    private String username;
+    private String password;
+}
